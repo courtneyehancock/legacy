12d2bcc253c5c90f64aeb6c8de07a505aeab4ef5
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-28 11:03:02 -0700

    Add fonts

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i&display=swap" rel="stylesheet" />
   <title><?php bloginfo('name'); ?></title>
   <?php wp_head(); ?>
 </head>
