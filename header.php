185837451335e4dedefa1d078250a6e12e086c24
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-28 10:25:32 -0700

    Edit header code

--- header.php
+++ header.php
@@ -3,36 +3,35 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i&display=swap" rel="stylesheet" />
   <title><?php bloginfo('name'); ?></title>
-  <!-- link CSS file -->
-  <link rel="stylesheet" type="text/css" href="<//?php bloginfo('stylesheet_url'); ?>" />
   <?php wp_head(); ?>
 </head>
 
 <body>
 
-  <header>
-    <div class="container">
-      <div class="row header-flex">
-        <div id="site-logo">
-          <!--If/else for Logo and Site Title-->
-          <?php if(get_header_image() == '') { ?>
-            <h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
-            <?php
-          }else{?>
-            <a href="<?php echo home_url('/'); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
-            <?php
-          }
-          ?>
-        </div>
-        <div id="site-nav">
-          <!--Navigation-->
-          <?php wp_nav_menu(array(
-            'theme_location' => 'header-menu',
-            'container-class' => 'menu-header'
-            ));
-          ?>
-        </div>
+<header>
+  <div class="container">
+    <div class="row header-flex">
+      <div id="site-logo">
+        <!--If/else for Logo and Site Title-->
+        <?php if(get_header_image() == '') { ?>
+          <h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
+          <?php
+        }else{?>
+          <a href="<?php echo home_url('/'); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
+          <?php
+        }
+        ?>
+      </div>
+      <div id="site-nav">
+        <!--Navigation-->
+        <?php wp_nav_menu(array(
+          'theme_location' => 'header-menu',
+          'container-class' => 'menu-header'
+          ));
+        ?>
       </div>
     </div>
-  </header>
+  </div>
+</header>
