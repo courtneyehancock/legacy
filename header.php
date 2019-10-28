5890ff0fb4359fcc1dbbcd3b8534eadacc623f66
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-28 09:57:09 -0700

    Edit structure for wppusher test

--- header.php
+++ header.php
@@ -1,32 +1,29 @@
 <!DOCTYPE html>
- <html lan="en"> 
-<head> 
-  <meta charset="utf-8"/> 
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?></title>
+  <!-- link CSS file -->
+  <!--<link rel="stylesheet" type="text/css" href="<//?php bloginfo('stylesheet_url'); ?>" />-->
+  <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css"/>-->
+  <?php wp_head(); ?>
+</head>
 
-  <title><?php bloginfo('name'); ?> </title> 
-
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" /> 
-
-  <?php wp_head();?> 
-
-</head>  
 <body>
+
   <header>
-    <div class="container-fluid">
+    <div class="container">
       <div class="row">
-        <div class="col-lg-12">
-          <nav>
-            <?php
-              if(has_nav_menu('top-menu')){
-                wp_nav_menu (array('theme_location' => 'top-menu', 'container_class' => 'top-menu-class'));
-              }else{
-                echo "Please select a top menu through the dashboard";
-              }
-            ?>
-          </nav>
+        <div class="three columns">
+          <h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
+        </div>
+        <div class="nine columns">
+          <?php wp_nav_menu(array(
+            'theme_location' => 'header-menu',
+            'container-class' => 'menu-header'
+            ));
+          ?>
         </div>
 
-      </div>
-    </div>
-</header>
+  </header>
