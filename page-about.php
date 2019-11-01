012b542276c796f5e6b6685db9e769802290e785
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-01 10:29:07 -0700

    Edit left about text color

--- page-about.php
+++ page-about.php
@@ -5,12 +5,12 @@
 <div class="container-fluid about-page">
 
   <div class="row align-items-center">
-    <div class="col-md-4 text-center">
+    <div class="col-md-4">
       <!-- About Brief Text Widget-->
       <?php dynamic_sidebar('about-left-brief-info'); ?>
     </div>
     <div class="col-md-8 text-center">
-      
+
     </div>
   </div>
 </div>
