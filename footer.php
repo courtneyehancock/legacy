077b796ee0bdf429e9fb55271c4da10c19a29b6c
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-01 16:33:34 -0700

    Edit footer text and padding

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
 
     <div>
       <img class="footer-img" src="http://courtneyehancock.com/legacyhospitalityllc/wp-content/uploads/2019/11/footer_legacy.png" alt="footer image" />
-      <p>&copy; Site created 2019</p>
     </div>
 
   </div>
