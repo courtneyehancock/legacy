b11f58af51c9a4fe38ae7e04aa9a6699da36e558
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-01 10:52:33 -0700

    Add functions to about page

--- page-about.php
+++ page-about.php
@@ -13,6 +13,26 @@
 
     </div>
   </div>
+
+  <div class="row align-items-center">
+    <div class="col-md-8 text-center">
+
+    </div>
+    <div class="col-md-4">
+      <!-- About Brief Text Widget-->
+      <?php dynamic_sidebar('about-right-brief-info'); ?>
+    </div>
+  </div>
+
+  <div class="row align-items-center">
+    <div class="col-md-4">
+      <!-- About Brief Text Widget-->
+      <?php dynamic_sidebar('about-bottom-left-brief-info'); ?>
+    </div>
+    <div class="col-md-8 text-center">
+
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
