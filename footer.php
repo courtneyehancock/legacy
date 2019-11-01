65790b77f040c031d1ee6c550ec06ffd97ff75e5
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-01 13:14:32 -0700

    Add footer img

--- footer.php
+++ footer.php
@@ -1,41 +1,11 @@
 <footer>
   <div class="container">
-    <div class="row">
-      <div class="four columns">
-        <!--Logo/text widget-->
-        <?php dynamic_sidebar('left-footer'); ?>
-      </div>
 
-      <div class="two columns">
-        <!--Middle Left Space-->
-        <?php dynamic_sidebar('middle-left-footer'); ?>
-      </div>
-
-      <div class="three columns footer-pad-top ">
-        <!--Menu Title Widget-->
-        <?php dynamic_sidebar('menu-title-footer'); ?>
-        <!-- Navigation -->
-        <?php wp_nav_menu(array(
-          'theme_location' => 'footer-menu',
-          'container-class' => 'menu-footer'
-          ));
-        ?>
-      </div>
-
-      <div class="three columns footer-pad-top ">
-        <!--Contact Widget-->
-        <?php dynamic_sidebar('right-footer'); ?>
-        <!--Social Widget-->
-        <?php dynamic_sidebar('social-footer'); ?>
-      </div>
+    <div>
+      <img class="footer-img" src="http://courtneyehancock.com/legacyhospitalityllc/wp-content/uploads/2019/11/footer_legacy.png" alt="footer image" />
+      <p>&copy; Site created 2019</p>
     </div>
 
-    <div class="row">
-      <div class="twelve columns">
-        <!--Copyright-->
-        <p>&copy; Site created 2019</p>
-      </div>
-    </div>
   </div>
 </footer>
 
