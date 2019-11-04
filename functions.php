30e9d343e8acf085650f738702c5cdd241c9799c
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-04 12:16:04 -0800

    Add business card function to contact template

--- functions.php
+++ functions.php
@@ -194,36 +194,17 @@
       'before_title'  => '<h3 class="widget-title">',
       'after_title'   => '</h3>'
     ));
-    //Contact: Contact Form Widget
+    //Contact: Business Card Widget
     register_sidebar(array(
-      'name'          => ('Contact Form'),
-      'id'            => 'contact-form',
-      'description'   => 'Contact form widget area in contact page',
-      'before_widget' => '<div class="widget-contact widget-form">',
+      'name'          => ('Business Card Contact'),
+      'id'            => 'business-card-contact',
+      'description'   => 'Business card widget area in contact Page',
+      'before_widget' => '<div class="business-card-widget">',
       'after_widget'  => '</div>',
       'before_title'  => '<h3 class="widget-title">',
       'after_title'   => '</h3>'
     ));
-    //Contact: Left Contact Widget
-    register_sidebar(array(
-      'name'          => ('Left Contact'),
-      'id'            => 'left-contact',
-      'description'   => 'Left widget area in contact Page',
-      'before_widget' => '<div class="widget-contact widget-left">',
-      'after_widget'  => '</div>',
-      'before_title'  => '<h3 class="widget-title">',
-      'after_title'   => '</h3>'
-    ));
-    //Contact: Right Contact Widget
-    register_sidebar(array(
-      'name'          => ('Right Contact'),
-      'id'            => 'right-contact',
-      'description'   => 'Right widget area in contact Page',
-      'before_widget' => '<div class="widget-contact widget-right">',
-      'after_widget'  => '</div>',
-      'before_title'  => '<h3 class="widget-title">',
-      'after_title'   => '</h3>'
-    ));
+
     //Footer: Left Widget
     register_sidebar(array(
       'name'          => ('Left Footer'),
--- page-contact.php
+++ page-contact.php
@@ -9,6 +9,13 @@
       <?php dynamic_sidebar('banner-contact'); ?>
     </div>
   </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <!--Banner Img Widget-->
+      <?php dynamic_sidebar('business-card-contact'); ?>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
