ed5849881235338b64e6b66a3f1c84bd2b463a41
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-04 07:21:27 -0800

    Edit footer image

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <div class="container">
 
     <div>
-      <img class="footer-img" src="http://courtneyehancock.com/legacyhospitalityllc/wp-content/uploads/2019/11/footer_legacy.png" alt="footer image" />
+      <img class="footer-img" src="http://courtneyehancock.com/legacyhospitalityllc/wp-content/uploads/2019/11/legacy_footer.png" alt="footer image" />
     </div>
 
   </div>
