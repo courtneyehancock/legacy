4164d0d544d8e5ee5b51b91bc2a5e48f83504c69
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-19 09:55:31 -0800

    Take away drop shadow

--- page-about.php
+++ page-about.php
@@ -5,18 +5,18 @@
 <div class="container-fluid about-page">
 
   <div class="row align-items-center">
-    <div class="col-md-4 shadow p-3 mb-5 bg-white">
+    <div class="col-md-6">
       <!-- About Brief Text Widget-->
       <?php dynamic_sidebar('about-left-brief-info'); ?>
     </div>
-    <div class="col-md-8">
+    <div class="col-md-6">
       <!-- About Right Img Widget-->
       <?php dynamic_sidebar('about-right-brief-info'); ?>
     </div>
   </div>
 
   <div class="row align-items-center">
-    <div class="shadow p-3 mb-5 bg-white">
+    <div>
       <!-- About Brief Text Widget-->
       <?php dynamic_sidebar('about-bottom-left-brief-info'); ?>
     </div>
