6a07c7440a1f17e28ff3147cea465ef63b0e63fd
Courtney Hancock <ceh.courtney@gmail.com>
2019-11-19 09:43:40 -0800

    Add drop shadow to about widgets

--- page-about.php
+++ page-about.php
@@ -5,18 +5,18 @@
 <div class="container-fluid about-page">
 
   <div class="row align-items-center">
-    <div class="col-md-4 pt-5">
+    <div class="col-md-4 shadow p-3 mb-5 bg-white">
       <!-- About Brief Text Widget-->
       <?php dynamic_sidebar('about-left-brief-info'); ?>
     </div>
-    <div class="col-md-8 text-center">
+    <div class="col-md-8">
       <!-- About Right Img Widget-->
       <?php dynamic_sidebar('about-right-brief-info'); ?>
     </div>
   </div>
 
   <div class="row align-items-center">
-    <div class="col-md-12">
+    <div class="shadow p-3 mb-5 bg-white">
       <!-- About Brief Text Widget-->
       <?php dynamic_sidebar('about-bottom-left-brief-info'); ?>
     </div>
